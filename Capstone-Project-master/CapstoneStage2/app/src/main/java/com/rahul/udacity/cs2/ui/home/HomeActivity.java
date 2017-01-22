4352eb10c6227117ebc50b38d245d1751c8afa95
rg_android <android.developer4444@gmail.com>
2017-01-22 08:59:09 +0530

    fix: standard back nav

--- Capstone-Project-master/CapstoneStage2/app/src/main/java/com/rahul/udacity/cs2/ui/home/HomeActivity.java
+++ Capstone-Project-master/CapstoneStage2/app/src/main/java/com/rahul/udacity/cs2/ui/home/HomeActivity.java
@@ -39,7 +39,8 @@ public class HomeActivity extends BaseActivity implements HomeView {
         if (mDrawerLayout.isDrawerOpen(GravityCompat.START))
             mDrawerLayout.closeDrawers();
         else
-            showConfirmationDialog();
+            super.onBackPressed();
+//            showConfirmationDialog();
     }
 
     @Override
